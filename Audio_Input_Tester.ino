38c738272948109b9849f383dda5694235c68eae
G6EJD <d.l.bird@sky.com>
2017-12-15 23:36:46 +0000

    Add files via upload

---
+++ Audio_Input_Tester.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(256000);
+}
+
+void loop() {
+  // If using an electret microphone unit, then it needs adjusting
+  // Select the Tools/Serial Plotter and use the display as an oscilloscope
+  // Don't forget to select the correct speed! 256000
+  // Connect audio to A0 or VP or VN
+  Serial.println(analogRead(A0));
+}
